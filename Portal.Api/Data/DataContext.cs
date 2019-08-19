7c5261d8243f9e8825584e9a30ccf2b577bffea0
Michal Gawlik <gawlos87@gmail.com>
2019-08-19 05:37:44 +0200

    Utworzenie pierwszego modelu Value

---
+++ Portal.Api/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using Portal.Api.Models;
+
+namespace Portal.Api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+        public DbSet<Value> Values { get; set; }
+        
+    }
+}
