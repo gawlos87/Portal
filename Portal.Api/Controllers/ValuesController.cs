a4272cd8a4a8794bb2fea023aba1c401f26fb742
Michal Gawlik <gawlos87@gmail.com>
2019-09-02 05:45:45 +0200

    add RUD

--- Portal.Api/Controllers/ValuesController.cs
+++ Portal.Api/Controllers/ValuesController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Portal.Api.Data;
+using Portal.Api.Models;
 
 namespace Portal.Api.Controllers
 {
@@ -11,36 +13,56 @@ namespace Portal.Api.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        private readonly DataContext _dbContext;
+
+        public ValuesController (DataContext DBContext)
+        {
+            _dbContext = DBContext;
+        }
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public IActionResult GetValues()
         {
-            return new string[] { "value1", "value2" };
+            var values = _dbContext.Values.ToList();
+            return Ok(values);
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public IActionResult GetValue(int id)
         {
-            return "value";
+            var value = _dbContext.Values.FirstOrDefault(x =>x.Id == id);
+            return Ok(value);
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value)
-        {
+        public IActionResult AddValue([FromBody] Value value)
+        {            
+            _dbContext.Values.Add(value);
+            _dbContext.SaveChanges();
+            return Ok();
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public IActionResult EditValue(int id, [FromBody] Value value)
         {
+            var data = _dbContext.Values.Find(id);
+            data.Name = value.Name;
+            _dbContext.Values.Update(data);
+            _dbContext.SaveChanges();
+            return Ok(data);
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(int id)
         {
+            var data = _dbContext.Values.Find(id);
+            _dbContext.Values.Remove(data);
+            _dbContext.SaveChanges();
+            return Ok(data);
         }
     }
 }
