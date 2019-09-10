8caa73a1cb31bd376757fc9a2b66906d75f231e6
Michal Gawlik <gawlos87@gmail.com>
2019-09-10 06:05:36 +0200

    validation

--- Portal.Api/Dtos/UserForRegisterDto.cs
+++ Portal.Api/Dtos/UserForRegisterDto.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Portal.Api.Dtos
 {
     public class UserForRegisterDto
     {
+        [Required(ErrorMessage="Nazwa użytkownika jest wymagana")]
         public string Username { get; set; }
+        [Required(ErrorMessage="Hasło jest wymagane")]
+        [StringLength(12,MinimumLength=6,ErrorMessage="Hasło musi sie składać z 6 do 12 znaków")]
         public string  Password { get; set; }
     }
 }
