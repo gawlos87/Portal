dd0e4626648df3e3973047120a6ec04c79805c5b
Michal Gawlik <gawlos87@gmail.com>
2019-09-11 05:23:42 +0200

    add auth-login

--- Portal.Api/Controllers/AuthController.cs
+++ Portal.Api/Controllers/AuthController.cs
@@ -1,19 +1,27 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using Portal.Api.Data;
 using Portal.Api.Dtos;
 using Portal.Api.Models;
 
 namespace Portal.Api.Controllers
-{    [Route("api/[controller]")]
+{   [Route("api/[controller]")]
     [ApiController]
     public class AuthController : ControllerBase
     {
         private readonly IAuthRepository _repository;
+        private readonly IConfiguration _config;
 
-        public AuthController(IAuthRepository repository)
+        public AuthController(IAuthRepository repository, IConfiguration config)
         {
             _repository = repository;
+            _config = config;
         }
 
         [HttpPost("register")]
@@ -32,6 +40,38 @@ namespace Portal.Api.Controllers
             var createdUser  = await _repository.Register(userToCreate, userForRegister.Password);
 
             return StatusCode(201);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
+        {
+            var userFromRepo = await _repository.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
+
+            if(userFromRepo == null)
+                return Unauthorized();
+
+            // Create Token
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                new Claim(ClaimTypes.Name, userFromRepo.Username)
+            };
+ 
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddHours(12),
+                SigningCredentials = creds
+            };
+
+            var tokenHendler = new JwtSecurityTokenHandler();
+            var token = tokenHendler.CreateToken(tokenDescriptor);
+
+            return Ok(new { token = tokenHendler.WriteToken(token)});
         }
     }
 }
