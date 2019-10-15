a59c22d2517115cd8aa9769cb54abafce11de273
Michal Gawlik <gawlos87@gmail.com>
2019-10-15 06:16:20 +0200

    add userlist component

--- Portal.SPA/src/app/app.module.ts
+++ Portal.SPA/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { JwtModule } from '@auth0/angular-jwt';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -9,23 +10,37 @@ import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { AlertifyService } from './_services/alertify.service';
+import { UserService } from './_services/user.service';
+import { UsersListComponent } from './users/users-list/users-list.component';
 
+export function tokeGetter() {
+   return localStorage.getItem('token');
+}
 
 @NgModule({
    declarations: [
       AppComponent,
       NavComponent,
       HomeComponent,
-      RegisterComponent
+      RegisterComponent,
+      UsersListComponent
    ],
    imports: [
       BrowserModule,
       HttpClientModule,
-      FormsModule
+      FormsModule,
+      JwtModule.forRoot({
+         config: {
+            tokenGetter: tokeGetter,
+            whitelistedDomains: ['localhost:5000'], 
+            blacklistedRoutes: ['localhost:5000/api/auth']
+         }
+      })
    ],
    providers: [
       AuthService,
-      AlertifyService
+      AlertifyService,
+      UserService
    ],
    bootstrap: [
       AppComponent
